189f5adc955bd35a9a8920645411509c776a6ee8
Em Karisch <em@khaos.solutions>
2025-09-19 12:09:15 +0200

    feat: overhaul technology for production and consumption combinators (#2)

--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,5 @@
 -- Better subgroup placement with SchallCircuitGroup
 require("data-updates.schall-circuit-group")
+
+-- Move combinator recips to advanced combinators tech, removes custom tech
+require("data-updates.tech-overhaul")
---
+++ data-updates/tech-overhaul.lua
@@ -0,0 +1,4 @@
+data.raw.technology["statistics-combinator"] = nil
+
+table.insert(data.raw.technology["advanced-combinators"].effects, {type = "unlock-recipe", recipe = "production-combinator"})
+table.insert(data.raw.technology["advanced-combinators"].effects, {type = "unlock-recipe", recipe = "consumption-combinator"})
