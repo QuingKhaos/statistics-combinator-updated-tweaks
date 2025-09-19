6b921ee75ea22115662c9b7f6fac9830602ede01
Em Karisch <em@khaos.solutions>
2025-09-19 11:52:42 +0200

    feat: add support for SchallCircuitGroup (#1)

---
+++ data-updates.lua
@@ -0,0 +1,2 @@
+-- Better subgroup placement with SchallCircuitGroup
+require("data-updates.schall-circuit-group")
---
+++ data-updates/schall-circuit-group.lua
@@ -0,0 +1,4 @@
+if mods["SchallCircuitGroup"] then
+  data.raw.item["production-combinator"].subgroup = "circuit-input"
+  data.raw.item["consumption-combinator"].subgroup = "circuit-input"
+end
